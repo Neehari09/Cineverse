222d73b0c2f48937d5b5d336a0f6695f624becd8
Neehari09 <neeharisoule@gmail.com>
2026-08-18 20:13:48 +0530

    Fix root path to return welcome JSON

--- backend_core/backend_core/urls.py
+++ backend_core/backend_core/urls.py
@@ -16,8 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.http import JsonResponse
+
+def home_view(request):
+    return JsonResponse({"message": "Welcome to the Cineverse API!"})
 
 urlpatterns = [
+    path('', home_view, name='home'),
     path('admin/', admin.site.urls),
     path('api/auth/', include('users.urls')),
     path('api/', include('api.urls')),
